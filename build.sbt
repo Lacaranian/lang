9f19121b5b881838fde9f61b1963b70b2c8f477e
Ben Elliott <benjam.elliott@gmail.com>
2020-05-17 12:28:07 -0600

    Add Unit Testing And a Linting Plugin
    For safety, get a few unit tests in place, that can track regressions more
    quickly than a human bean or any other legume.
    
    For more safety, add a plugin that can lint for common Scala pitfalls (a
    == b where A and B are of strictly disjoint types).

--- build.sbt
+++ build.sbt
@@ -5,3 +5,11 @@ version := "0.1"
 scalaVersion := "2.13.2"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
+
+// Testing
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.1.1"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1" % "test"
+
+logBuffered in Test := false
+
+resolvers += "Artima Maven Repository" at "https://repo.artima.com/releases"
---
+++ src/test/scala/com/schemagames/lang/parser/TokenLexer.scala
@@ -0,0 +1,43 @@
+package com.schemagames.lang.parser
+
+import com.schemagames.lang.syntax.Tokens.{Indent, Newline, OpenBlock, Outdent}
+import org.scalatest._
+import org.scalatest.matchers.should.Matchers
+
+class TokenLexerSpec extends FlatSpec with Matchers {
+
+  "The TokenLexer" should "be able to turn a valid input program into tokens" in {
+    val program =
+      """def testProgram = {
+        |  def floop = 123
+        |  def bloop = floop
+        |  def noop = "nope"
+        |
+        |  noop
+        |}
+        |
+        |def main = testProgram
+        |""".stripMargin
+
+    val results = TokenLexer(program)
+    results shouldBe a [Right[_, _]]
+  }
+
+  it should "recognize indentation and outdentation after newlines" in {
+    val program =
+      """def program = {
+        |  def thing = 1
+        |}""".stripMargin
+    val results = TokenLexer(program)
+    results shouldBe a [Right[_, _]]
+
+    val Right(tokens) = results
+    val afterFirstNewline = tokens.dropWhile(_ != Newline()).drop(1)
+
+    afterFirstNewline.headOption should be (Some(Indent()))
+
+    val afterSecondNewline = afterFirstNewline.dropWhile(_ != Newline()).drop(1)
+
+    afterSecondNewline.headOption should be (Some(Outdent()))
+  }
+}
