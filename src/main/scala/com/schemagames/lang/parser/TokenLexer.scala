4f70aec501b176eef76131e3534e190f5a52045a
Ben Elliott <benjam.elliott@gmail.com>
2020-05-17 12:24:40 -0600

    Add Newline/Indentation Tokens
    Add to the TokenLexer the ability to recognize levels of indentation from
    line to line, and parse the combination of newlines and indentation into
    tokens. Later split these, as the grouping was mainly for associating the
    two.
    
    This temporarily breaks the AST parser, but will be helpful in adding
    delimiting of expressions, optionally with a delimiter character such as a
    semicolon.

--- src/main/scala/com/schemagames/lang/parser/TokenLexer.scala
+++ src/main/scala/com/schemagames/lang/parser/TokenLexer.scala
@@ -1,6 +1,6 @@
 package com.schemagames.lang.parser
 
-import com.schemagames.lang.syntax.Token
+import com.schemagames.lang.syntax.{IndentationType, Spaces, Tabs, Token}
 import com.schemagames.lang.syntax.Tokens._
 
 import scala.util.matching.Regex
@@ -14,13 +14,21 @@ object TokenLexer extends RegexParsers {
     }
   }
 
-  // Skip most whitespace, but not \n newlines
-  //override def skipWhitespace: Boolean = true
-  //override val whiteSpace: Regex = "[ \t\r\f]+".r
+  // Skip most leading whitespace, but not \n newlines
+  // Also, don't skip leading whitespace when lexing the first indentation on a line
+  override def skipWhitespace: Boolean = skipWhiteSpaceOverride.getOrElse(true)
+  var skipWhiteSpaceOverride: Option[Boolean] = None
+  override val whiteSpace: Regex = "[ \t\r\f]+".r
 
   def tokens: Parser[List[Token]] = phrase(rep1(
-    assign | openBlock | closeBlock | `def` | numLiteral | stringLiteral | identifier
-  )) ^^ { rawTokens => rawTokens }
+    assign | openBlock | closeBlock | `def` | numLiteral | stringLiteral | identifier | newlineWithIndentation
+  )) >> { rawTokens => {
+    val splitTokens = splitOutNewlinesWithIndentation(rawTokens)
+    processIndentation(splitTokens) match {
+      case Left(errMsg) => err(errMsg)
+      case Right(tokens) => success(tokens)
+    }
+  }}
 
   def identifier: Parser[Identifier] = positioned {
     "[a-zA-Z_][a-zA-Z0-9_]*".r ^^ { str => Identifier(str) }
@@ -38,5 +46,105 @@ object TokenLexer extends RegexParsers {
   def closeBlock: Parser[CloseBlock] = positioned("}" ^^ (_ => CloseBlock()))
   def `def`: Parser[Def] = positioned("def" ^^ (_ => Def()))
   def assign: Parser[Assign] = positioned("=" ^^ (_ => Assign()))
-  //def newline: Parser[Newline] = positioned("\n".r ^^ (_ => Newline()))
+
+  def newlineWithIndentation: Parser[NewlineWithIndentation] = newline ~ indentation ^^ {
+    case newline ~ indent => NewlineWithIndentation(newline, indent)
+  }
+  def newline: Parser[Newline] = positioned("\n" ^^ (_ => Newline()))
+  def indentation: Parser[Indentation] = withLeadingWhitespace(
+    positioned(indentationBySpaces | indentationByTabs | indentationByMixedWhitespace)
+  )
+  def indentationBySpaces: Parser[Indentation] = withLeadingWhitespace("[ ]*".r) ^^ { str => // Just spaces
+    Indentation(str.length, Spaces)
+  }
+  def indentationByTabs: Parser[Indentation] = withLeadingWhitespace("[\t]*".r) ^^ { str => // Just tabs
+    Indentation(str.length, Tabs)
+  }
+  // Always fails, as this is explicitly prohibited
+  def indentationByMixedWhitespace: Parser[Nothing] = (withLeadingWhitespace("[ \t]*".r) ^^ { str => // Mixed whitespace - this is a failure!
+    // TODO: Single pass count
+    val numSpaces = str.count(_ == ' ')
+    val numTabs = str.count(_ == '\t')
+
+    (numSpaces, numTabs)
+  }) >> { case (numSpaces, numTabs) =>  failure(
+    s"expected indentation with only spaces or only tabs, got $numSpaces spaces and $numTabs tabs"
+  ) }
+
+  def delimit: Parser[Delimit] = positioned("[;\n]".r ^^ (_ => Delimit()))
+
+  def splitOutNewlinesWithIndentation(tokens: List[Token]): List[Token] = tokens.flatMap{
+    case NewlineWithIndentation(n, i) => List(n, i)
+    case other => List(other)
+  }
+
+  // Convert Indentation of specific amounts into indents and outdents
+  // If within a given "indentation block" we find mixed indentation types, the parser fails
+  // TODO: Avoid ..., Outdent(), Indent(), ...
+  def processIndentation(
+    tokens: List[Token],
+    indentStack: List[(Int, IndentationType)] = Nil //(amt > 0, indent type)
+  ): Either[String, List[Token]] = {
+    tokens.headOption match {
+      case None => Right(indentStack.map(_ => Outdent()))
+      case Some(Indentation(amt, indentType)) => indentStack.headOption match {
+        // Mixed indentation
+        case Some((_, prevIndentType)) if prevIndentType != indentType => Left(
+          "Line found with indentation characters mismatching those from the previous indentation!"
+        )
+        // Indenting further out
+        case Some((prevAmt, _)) if amt > prevAmt => for {
+          indentedTokens <- processIndentation(tokens.drop(1), (amt, indentType) :: indentStack)
+        } yield Indent() :: indentedTokens
+        // Outdenting back in
+        case Some((prevAmt, _)) if amt < prevAmt => {
+          val (dropped, kept) = indentStack.partition(_._1 > amt)
+          for {
+            indentedTokens <- processIndentation(tokens.drop(1), kept)
+          } yield dropped.map(_ => Outdent()) ++ indentedTokens
+        }
+        // Staying at current indentation
+        case Some((prevAmt, _)) if amt == prevAmt => for {
+          indentedTokens <- processIndentation(tokens.drop(1), indentStack)
+        } yield indentedTokens
+        // Special case of indenting from no previous indentation
+        case None if amt > 0 => for {
+          indentedTokens <- processIndentation(tokens.drop(1), (amt, indentType) :: indentStack)
+        } yield Indent() :: indentedTokens
+        // Staying at no indentation
+        case None => for {
+          indentedTokens <- processIndentation(tokens.drop(1), indentStack)
+        } yield indentedTokens
+      }
+      // Other tokens are just kept without any indentation tokens replacing them
+      case Some(otherToken) => for {
+        indentedTokens <- processIndentation(tokens.drop(1), indentStack)
+      } yield otherToken :: indentedTokens
+    }
+  }
+
+  def withLeadingWhitespace[A](p: Parser[A]): Parser[A] = { (in: Input) => {
+    val origSkipWhiteSpaceOverride = skipWhiteSpaceOverride
+    skipWhiteSpaceOverride = Some(false)
+    val result = p.apply(in)
+    skipWhiteSpaceOverride = origSkipWhiteSpaceOverride
+    result
+  }}
+}
+
+// Cribbed from scala.util.parsing.combinator, as it was private
+private class SubSequence(s: CharSequence, start: Int, val length: Int) extends CharSequence {
+  def this(s: CharSequence, start: Int) = this(s, start, s.length - start)
+
+  def charAt(i: Int) =
+    if (i >= 0 && i < length) s.charAt(start + i) else throw new IndexOutOfBoundsException(s"index: $i, length: $length")
+
+  def subSequence(_start: Int, _end: Int) = {
+    if (_start < 0 || _end < 0 || _end > length || _start > _end)
+      throw new IndexOutOfBoundsException(s"start: ${_start}, end: ${_end}, length: $length")
+
+    new SubSequence(s, start + _start, _end - _start)
+  }
+
+  override def toString = s.subSequence(start, start + length).toString
 }
--- src/main/scala/com/schemagames/lang/syntax/Tokens.scala
+++ src/main/scala/com/schemagames/lang/syntax/Tokens.scala
@@ -9,9 +9,22 @@ object Tokens {
   case class StringLiteral(str: String) extends Token
   case class NumLiteral(str: String) extends Token
 
-  case class OpenBlock()  extends Token // {
-  case class CloseBlock() extends Token // }
-  case class Def()        extends Token // def
-  case class Assign()     extends Token // =
-  //case class Newline()    extends Token // \n
+  case class OpenBlock()   extends Token // {
+  case class CloseBlock()  extends Token // }
+  case class Def()         extends Token // def
+  case class Assign()      extends Token // =
+  case class Newline()     extends Token // \n
+
+  case class Delimit()     extends Token // ; \n
+
+  case class NewlineWithIndentation(n: Newline, i: Indentation) extends Token
+  case class Indentation(num: Int, indentationType: IndentationType) extends Token // \n plus some number of spaces/tabs...
+  case class Indent()         extends Token // ... greater then the previous line's
+  case class Outdent()        extends Token // ... less than the previous line's
+
 }
+
+// Mixing indentation types is a lexer error
+sealed trait IndentationType
+case object Tabs   extends IndentationType
+case object Spaces extends IndentationType
